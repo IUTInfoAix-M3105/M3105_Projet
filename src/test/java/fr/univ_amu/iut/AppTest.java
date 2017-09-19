153e2d3f99c0aaa3bf01e3cf665dcea197c34e31
Sébastien NEDJAR <sebastien@nedjar.com>
2017-09-19 15:15:06 +0200

    ajout de la structure du projet Maven

---
+++ src/test/java/fr/univ_amu/iut/AppTest.java
@@ -0,0 +1,11 @@
+package fr.univ_amu.iut;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void test_should_fail() {
+        assertTrue(true,"Really serious test !!!");
+    }
+}
