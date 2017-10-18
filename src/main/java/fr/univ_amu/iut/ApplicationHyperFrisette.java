958477623be9b458e35b205d1124600fd5ab0b12
Sébastien NEDJAR <sebastien@nedjar.com>
2017-10-18 08:53:12 +0200

    Création d'un uberJar Executable par défaut

---
+++ src/main/java/fr/univ_amu/iut/ApplicationHyperFrisette.java
@@ -0,0 +1,9 @@
+package fr.univ_amu.iut;
+
+ 
+public class ApplicationHyperFrisette {
+	public static void main(String[] args) {
+		System.out.println("Plop !");
+	}
+}
+
--- src/test/java/fr/univ_amu/iut/ApplicationHyperFrisetteTest.java
+++ src/test/java/fr/univ_amu/iut/ApplicationHyperFrisetteTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public class AppTest {
+public class ApplicationHyperFrisetteTest {
     @Test
     public void test_should_fail() {
         assertTrue("Really serious test !!!", true);
